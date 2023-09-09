504301f5acd8718090b7df1dce7cde93db766211
Lucas O.Gonçalves <134063912+lucastuia@users.noreply.github.com>
2023-09-09 13:00:53 -0300

    aaa

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-function carConteudo() {
-    document.getElementById("conteudo").innerHTML = "<p> quickly add Bootstrap to your project? Use jsDelivr, a free open source CDN. Using a package man</p>";
-}
+$(document) .ready(function(){
+    //adicionar o código javascript - jquery dentro do document.ready
+    $('#btnClique').click(function(){
+       //acao do clique no elemento definido
+       alert('');
 
+       //variavel
+       var nome=prompt('Digite seu nome');
+       alert(nome);
+       $('mensagem').html('<div class="alert alert-warning" role="alert">'+nome+'</div>');
+    });
+});
+
+$(document) .ready(function(){
+    //adicionar o código javascript - jquery dentro do document.ready
+    $('#btnClique').click(function(){
+       //acao do clique no elemento definido
+       alert('dffddddcd');
+
+       //variavel
+       var nome=prompt('Digite seu nome');
+       alert(nome);
+       $('jonas').html('<div class="alert alert-warning" role="alert">'+nome+'</div>');
+    });
+});
