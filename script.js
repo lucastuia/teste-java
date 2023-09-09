173ce399e1fc45d74d32ff96da6d89f232908d03
Lucas O.Gonçalves <134063912+lucastuia@users.noreply.github.com>
2023-09-08 21:24:48 -0300

    teste java

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function carConteudo() {
-    document.getElementById("conteudo").innerHTML = <p>assdfghfgffdsaertghnvbxfdfrgdhmnbftryfhjnbfgrwtyjhgtreyfghfstertydghfgtstry</p>;
-
+    document.getElementById("conteudo").innerHTML = "<p> quickly add Bootstrap to your project? Use jsDelivr, a free open source CDN. Using a package man</p>";
 }
+
