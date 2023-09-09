4f5ab1ab48b245744472755b2cd236ddba62eeac
Lucas O.Gonçalves <134063912+lucastuia@users.noreply.github.com>
2023-09-08 21:05:21 -0300

    aaaaaa teste

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-//obrigatorio add no código javascript - jquery
-$(document) .ready(function(){
-    //adicionar o código javascript - jquery dentro do document.ready
-    $('#btnClique').click(function(){
-       //acao do clique no elemento definido
-       alert('lucas da porra loca');
+function carConteudo() {
+    document.getElementById("conteudo").innerHTML = <p>assdfghfgffdsaertghnvbxfdfrgdhmnbftryfhjnbfgrwtyjhgtreyfghfstertydghfgtstry</p>;
 
-       //variavel
-       var nome=prompt('Digite seu nome');
-       alert(nome);
-       $('mensagem').html('<div class="alert alert-warning" role="alert">'+nome+'</div>');
-    });
-});
+}
